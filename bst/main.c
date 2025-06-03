9e37a5557b48fcd83c354fd042ba4aa5cfd91442
Victor Saraiva <victor_alex2005@hotmail.com>
2025-06-03 15:46:48 -0300

    adição de espaços para melhor formatação

--- bst/main.c
+++ bst/main.c
@@ -42,7 +42,7 @@ int main(void) {
       
     case 7:
     	scanf("%d", &valor_iterativo);
-	printf("%d\n", sucessor(valor_iterativo, arvore));
+			printf("%d\n", sucessor(valor_iterativo, arvore));
     }
   } while (funcao != 99);
 }
